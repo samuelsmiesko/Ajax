d63e84c13ac47d17969e1b2ac9857dce62ef7d48
samoprogramuje <samuel1820@gmail.com>
2023-11-04 09:55:21 +0100

    4.11.2023

--- index.php
+++ index.php
@@ -4,10 +4,11 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link rel="shortcut icon" type="x-icon" href="image.png">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <title>Document</title>
+    <title>Search Data</title>
 
 </head>
 <body>
